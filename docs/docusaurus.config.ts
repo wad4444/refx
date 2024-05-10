4c78c2c69bfb4818dbb2b761a0ba31c35390f5ca
g1mmethemoney <theguywholovecheetos@gmail.com>
2024-05-10 20:06:32 +0300

    fix: remove edit this page links

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -38,15 +38,6 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: './src/css/custom.css',
